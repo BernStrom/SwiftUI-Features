2f443f3d0e2b0491123bc3ce725a896d5cdc5d96
Bern <bernnystrom@gmail.com>
2022-06-11 20:01:41 +0800

    🎉 (new): Add new feature project on SwiftUI Animations
    Tested out more advanced animation features in SwiftUI - Controlling animation stack and animating drag gestures.

---
+++ Animations2/Animations2/Animations2App.swift
@@ -0,0 +1,17 @@
+//
+//  Animations2App.swift
+//  Animations2
+//
+//  Created by Bern N on 6/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Animations2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
