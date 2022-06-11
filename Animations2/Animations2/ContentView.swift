5699d7a2a7769aa34b0e6005b14866cf09af0249
Bern <bernnystrom@gmail.com>
2022-06-12 00:53:30 +0800

    ✨ (new): Add transition modifier feature example
    Tested out showing and hiding views with the transition modifier feature.

--- Animations2/Animations2/ContentView.swift
+++ Animations2/Animations2/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     let letters = Array("Hello SwiftUI")
+    @State private var isShowingRed = false
     @State private var enabled = false
     @State private var dragAmount = CGSize.zero
     
@@ -57,6 +58,21 @@ struct ContentView: View {
             .foregroundColor(.white)
             .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
             .animation(.interpolatingSpring(stiffness: 5, damping: 1), value: enabled)
+            
+            VStack {
+                Button("Tap Me") {
+                    withAnimation {
+                        isShowingRed.toggle()
+                    }
+                }
+                
+                if isShowingRed {
+                    Rectangle()
+                        .fill(.red)
+                        .frame(width: 200, height: 200)
+                        .transition(.asymmetric(insertion: .scale, removal: .opacity))
+                }
+            }
         }
     }
 }
